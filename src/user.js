03af8b97915d58062af40e13dd23382d31bcff7a
Tilman Kamp <tilmankamp@posteo.de>
2018-02-15 14:17:26 +0100

    better user model

--- src/user.js
+++ src/user.js
@@ -2,28 +2,43 @@
 var exports = module.exports = {}
 
 exports.initDb = function(db) {
-    console.log(JSON.stringify(db))
-    console.log(!!db.users)
     if (!db.users) {
-        console.log('Was soll das?')
         db.users = { 'admin': { token: '' } }
     }
 }
 
 exports.initApp = function(app, db) {
-    
     app.get('/users', function(req, res) {
-        res.status(200).send({ users: db.users })
-        console.log(JSON.stringify(db.users))
+        res.status(200).send(Object.keys(db.users))
     })
 
-    app.get('/users/add', function(req, res) {
-        var id = req.query.id
-        if (db.users[id])
-            res.status(400).send()
-        else {
-            db.users[id] = { id: id, name: req.query.name, email: req.query.email }
+    app.get('/users/:id', function(req, res) {
+        var id = req.params.id
+        var user = db.users[id]
+        if (user) {
+            res.status(200).send({ id: id, name: user.name, email: user.email })
+        } else {
+            res.status(404).send()
+        }
+    })
+
+    app.put('/users/:id', function(req, res) {
+        var id = req.params.id
+        var user = JSON.parse(req.body)
+        if (db.users[id]) {
+            res.status(409).send()
+        } else {
+            db.users[id] = { id: id, name: user.name, email: user.email, password: user.password }
             res.status(200).send()
+        }
+    })
+
+    app.delete('/users/:id', function(req, res) {
+        var id = req.params.id
+        if (db.users[id]) {
+            delete db.users[id]
+        } else {
+            res.status(404).send()
         }
     })
 }
