052ab02f3b873ab0ba4c1eb96c1182d9973b4913
Tilman Kamp <5991088+tilmankamp@users.noreply.github.com>
2018-08-21 11:53:15 +0200

    Fix #106 - config env var

--- src/config.js
+++ src/config.js
@@ -2,18 +2,19 @@ const fs = require('fs')
 const path = require('path')
 const yaml = require('js-yaml')
 
-var filename = path.join('/etc/snakepit.conf')
+var filename = process.env.SNAKEPIT_CONF || '/etc/snakepit.conf'
 if (!fs.existsSync(filename)) {
     filename = path.join(process.env.HOME, '.snakepit', 'snakepit.conf')
 }
-var content
-try {
-    content = fs.readFileSync(filename, 'utf8')
-} catch (err) {
-    console.error('Unable to load config file from "' + filename + '"')
+
+function tryConfigFile(fun, verb) {
+    try {
+        return fun()
+    } catch (err) {
+        console.error('Problem ' + verb + ' config file "' + filename + '"')
+        process.exit(1)
+    }
 }
-try {
-    module.exports = yaml.safeLoad(content);
-} catch (err) {
-    console.error('Unable to parse content of config file "' + filename + '"')
-}
+
+var content = tryConfigFile(() => fs.readFileSync(filename), 'reading')
+module.exports = tryConfigFile(() => yaml.safeLoad(content), 'parsing')
