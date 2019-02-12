a9f3aaa87e9ec82296293fc866a59fbf37a98bff
Tilman Kamp <5991088+tilmankamp@users.noreply.github.com>
2019-02-12 17:28:06 +0100

    Using dhcpcd5, not executing run.sh scripts if not live

--- scripts/daemon/run.sh
+++ scripts/daemon/run.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [ "$HOSTNAME" = snakepit-daemon ]; then
+    exit 0
+fi
+
 pit_root="/data/rw/pit"
 touch ${pit_root}/daemon.log
 print_log () {
@@ -11,14 +15,6 @@ if [ -f "${pit_root}/run" ]; then
     touch "${pit_root}/stop"
 fi
 touch "${pit_root}/run"
-
-readarray -t interfaces < <(ip l | awk -F ":" '/^[0-9]+:/{dev=$2 ; if ( dev !~ /^ lo$/) {print $2}}')
-for i in "${interfaces[@]// /}" ; do
-        iface=`echo $i | cut -d'@' -f 1`
-        if [[ $iface == eth* ]] ; then
-            dhclient $iface || true
-        fi
-done
 
 for worker_dir in ${pit_root}/workers/*/ ; do
     worker_dir=${worker_dir%*/}
