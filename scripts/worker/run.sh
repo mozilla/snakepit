93c85190f4d78ebe51ec2bee55f77696a90c28c9
Tilman Kamp <5991088+tilmankamp@users.noreply.github.com>
2019-03-07 13:44:32 +0100

    Waiting for failed apt-daily

--- scripts/worker/run.sh
+++ scripts/worker/run.sh
@@ -49,7 +49,7 @@ print_log "Preparing script execution..."
 apt-get update 2>&1 | pipe_log
 systemctl stop apt-daily.service
 systemctl kill --kill-who=all apt-daily.service
-while ! (systemctl list-units --all apt-daily.service | fgrep -q dead) ; do sleep 1; done
+while ! (systemctl list-units --all apt-daily.service | grep -qE 'dead|failed') ; do sleep 1; done
 print_log "Starting script..."
 stdbuf -oL bash "/data/rw/pit/script.sh" 2>&1 | pipe_log
 exit_code=${PIPESTATUS[0]}
