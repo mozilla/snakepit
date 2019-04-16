7d63510732c026e28643a285fb809ecaa3fd407f
Tilman Kamp <5991088+tilmankamp@users.noreply.github.com>
2019-04-16 14:36:16 +0200

    Updated Sequelize

--- src/models/db.js
+++ src/models/db.js
@@ -6,6 +6,5 @@ module.exports = new Sequelize('snakepit', 'postgres', '', {
   host: 'localhost',
   dialect: 'postgres',
   pool: { max: 10, min: 0, acquire: 30000, idle: 10000 },
-  operatorsAliases: false,
   logging: false
 })
