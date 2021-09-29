2341385c73910959999ed9c243afdd223d0ad580
Andrew Erickson <aerickson@mozilla.com>
2021-09-29 15:56:10 -0700

    tweak env setup

---
+++ scripts/worker/proxy_env.sh
@@ -0,0 +1,9 @@
+export http_proxy="http://192.168.1.1:3128/"
+export https_proxy="http://192.168.1.1:3128/"
+export ftp_proxy="http://192.168.1.1:3128/"
+export no_proxy="localhost,127.0.0.1,::1,.mlc,192.168.1.1,10.2.224.243"
+
+export HTTP_PROXY=$http_proxy
+export HTTPS_PROXY=$https_proxy
+export FTP_PROXY=$ftp_proxy
+export NO_PROXY=$no_proxy
