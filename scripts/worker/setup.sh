e5162c47aaef0b1df7cd7ef71acf430b8d184f95
Andrew Erickson <aerickson@mozilla.com>
2021-09-29 17:06:50 -0700

    try installing nvidia stuff

--- scripts/worker/setup.sh
+++ scripts/worker/setup.sh
@@ -9,6 +9,10 @@ print_header () {
     printf "\n>>>>>>>> $1 <<<<<<<<\n\n"
 }
 
+print_header "Configuring APT repositories"
+echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list
+echo "deb http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/nvidia-machine-learning.list
+
 print_header "Installing dependencies"
 # # Official Nvidia Ubuntu PPA
 # aptget update
@@ -16,8 +20,10 @@ print_header "Installing dependencies"
 # # !This image and your GPU nodes should feature the very same driver!
 # add-apt-repository -y ppa:graphics-drivers/ppa
 aptget update
+# install ubuntu repo things
 aptget install dhcpcd5 sshfs vim iputils-ping npm nfs-common git
-# aptget install nvidia-driver-410 nvidia-utils-410 nvidia-cuda-toolkit
+# install nvidia repo things
+aptget install nvidia-driver-410 nvidia-utils-410 nvidia-cuda-toolkit
 
 print_header "Preparing apt"
 mv 20auto-upgrades /etc/apt/apt.conf.d/20auto-upgrades
