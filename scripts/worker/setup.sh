4884e4c89b7ff2149417b5933c0a41d22958f0c6
Andrew Erickson <aerickson@mozilla.com>
2021-09-30 12:59:53 -0700

    fix typo

--- scripts/worker/setup.sh
+++ scripts/worker/setup.sh
@@ -61,9 +61,9 @@ print_header "Configuring /etc/environment"
 echo 'http_proxy="http://192.168.1.1:3128/"' >> /etc/environment
 echo 'https_proxy="http://192.168.1.1:3128/"' >> /etc/environment
 echo 'ftp_proxy="http://192.168.1.1:3128/"' >> /etc/environment
-echo 'no_proxy="localhost,127.0.0.1,::1,.mlc,192.168.1.1,10.2.224.243"export http_proxy="http://192.168.1.1:3128/"' >> /etc/environment
+echo 'no_proxy="localhost,127.0.0.1,::1,.mlc,192.168.1.1,10.2.224.243"' >> /etc/environment
 # all caps versions of the above
 echo 'HTTP_PROXY="http://192.168.1.1:3128/"' >> /etc/environment
 echo 'HTTPS_PROXY="http://192.168.1.1:3128/"' >> /etc/environment
 echo 'FTP_PROXY="http://192.168.1.1:3128/"' >> /etc/environment
-echo 'NO_PROXY="localhost,127.0.0.1,::1,.mlc,192.168.1.1,10.2.224.243"export http_proxy="http://192.168.1.1:3128/"' >> /etc/environment
+echo 'NO_PROXY="localhost,127.0.0.1,::1,.mlc,192.168.1.1,10.2.224.243"' >> /etc/environment
